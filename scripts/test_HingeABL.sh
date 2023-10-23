35a1ffdc0d6c401505a2a4eb15f64bf15be42d47
Jize-W <1511160535@qq.com>
2023-10-23 13:40:02 +0800

    Add files via upload

---
+++ scripts/test_HingeABL.sh
@@ -0,0 +1,21 @@
+python train_sup.py --data_dir ./dataset/docred \
+--transformer_type bert \
+--model_name_or_path bert-base-cased \
+--train_file train_revised.json \
+--dev_file dev_revised.json \
+--test_file test_revised.json \
+--train_batch_size 4 \
+--test_batch_size 8 \
+--gradient_accumulation_steps 1 \
+--num_labels 4 \
+--learning_rate 5e-5 \
+--max_grad_norm 1.0 \
+--warmup_ratio 0.06 \
+--num_train_epochs 30 \
+--seed 233 \
+--num_class 97 \
+--loss_type HingeABL \
+--save_name redocred_bert_HingeABL_seed=233 \
+--proj_name redocred \
+--run_name bert_HingeABL \
+--load_path checkpoint/redocred_bert_HingeABL_seed=233.pt
---
+++ scripts/train_HingeABL.sh
@@ -0,0 +1,20 @@
+python train_sup.py --data_dir ./dataset/docred \
+--transformer_type bert \
+--model_name_or_path bert-base-cased \
+--train_file train_annotated.json \
+--dev_file dev.json \
+--test_file dev.json \
+--train_batch_size 4 \
+--test_batch_size 8 \
+--gradient_accumulation_steps 1 \
+--num_labels 4 \
+--learning_rate 5e-5 \
+--max_grad_norm 1.0 \
+--warmup_ratio 0.06 \
+--num_train_epochs 30 \
+--seed 66 \
+--num_class 97 \
+--loss_type HingeABL \
+--save_name docred_bert_HingeABL \
+--proj_name docred \
+--run_name bert_HingeABL
